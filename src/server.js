413592c6579a705ce57ea38c97b9eac14224120c
Andreas Møller <andreas.moller@gmail.com>
2017-09-18 15:37:17 +0200

    set json parse limit to 10mb

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 })
 
 app.use(cookieParser())
-app.use(bodyParser.json())
+app.use(bodyParser.json({limit: '10mb'}))
 app.use('/graphql', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
